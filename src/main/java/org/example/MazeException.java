ae93a9bed3e99082072138479c664b61695b5500
Mithra Sankar <mithra.sankar@gmail.com>
2023-05-12 09:30:12 -0400

    final with headers

--- src/main/java/org/example/MazeException.java
+++ src/main/java/org/example/MazeException.java
@@ -1,3 +1,9 @@
+/**
+ * <h1>Lab11</h1>
+ * <h2>CISC181-080L Spring 2023</h2>
+ * <h3>University of Delaware</h3>
+ * @author Mithra Sankar
+ */
 package org.example;
 
 public class MazeException extends RuntimeException {
